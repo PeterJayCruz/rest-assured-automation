fc3a1f6ba1bbdfab2b57ab43b9568e4ee33a8171
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-06-10 18:32:05 -0700

    added another create test user method to api utilities

--- src/test/java/org/peterjaycruz/realworld/api/utilities/ApiUtilities.java
+++ src/test/java/org/peterjaycruz/realworld/api/utilities/ApiUtilities.java
@@ -3,6 +3,7 @@ package org.peterjaycruz.realworld.api.utilities;
 import io.restassured.specification.RequestSpecification;
 import org.peterjaycruz.realworld.api.constants.Path;
 import org.peterjaycruz.realworld.api.request.UserRequestBody;
+import org.peterjaycruz.realworld.api.response.UserResponseBody;
 
 public class ApiUtilities {
 
@@ -13,5 +14,13 @@ public class ApiUtilities {
     RestAssuredUtilities.postRequest(requestSpec, "");
 
     return user;
+  }
+
+  public static UserResponseBody createTestUser(UserRequestBody user) {
+    RequestSpecification requestSpec = RestAssuredUtilities.createRequestSpecification(Path.REGISTRATION, user);
+    
+    return RestAssuredUtilities.postRequest(requestSpec, "")
+                                  .body()
+                                  .as(UserResponseBody.class);
   }
 }
