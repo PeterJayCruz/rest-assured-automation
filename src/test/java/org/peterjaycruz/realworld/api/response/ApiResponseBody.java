c4cbc72629a622ab7a6c3c777be639766c0f2875
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-07-15 17:17:05 -0700

    changed ApiResponseBody class  back to concrete class

--- src/test/java/org/peterjaycruz/realworld/api/response/ApiResponseBody.java
+++ src/test/java/org/peterjaycruz/realworld/api/response/ApiResponseBody.java
@@ -1,8 +1,5 @@
 package org.peterjaycruz.realworld.api.response;
 
-import java.util.HashMap;
+public class ApiResponseBody {
 
-public abstract class ApiResponseBody {
-
-  public abstract HashMap<String, String> getRoot();
 }
