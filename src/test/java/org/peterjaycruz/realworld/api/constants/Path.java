6073267b80e0dd384f50ba6bb6c95d91a8186ed3
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-07-19 09:07:21 -0700

    added string constant for /articles uri

--- src/test/java/org/peterjaycruz/realworld/api/constants/Path.java
+++ src/test/java/org/peterjaycruz/realworld/api/constants/Path.java
@@ -5,4 +5,5 @@ public class Path {
   public static final String BASE_URI = "http://localhost:8080";
   public static final String REGISTRATION = "/users";
   public static final String LOGIN = "/users/login";
+  public static final String ARTICLES = "/articles";
 }
