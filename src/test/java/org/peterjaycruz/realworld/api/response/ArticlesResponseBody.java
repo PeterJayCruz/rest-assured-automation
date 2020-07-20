4b4ce4fe9a8d06f8eb0a265dad370ccead7df440
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-07-20 10:31:36 -0700

    Articles response body pojo classes - changed fields to private and added getters and setters

--- src/test/java/org/peterjaycruz/realworld/api/response/ArticlesResponseBody.java
+++ src/test/java/org/peterjaycruz/realworld/api/response/ArticlesResponseBody.java
@@ -1,10 +1,37 @@
 package org.peterjaycruz.realworld.api.response;
 
+import java.util.ArrayList;
+
 public class ArticlesResponseBody extends ApiResponseBody {
 
-  public ResponseArticle article;
+  private ResponseArticle article;
 
   public ArticlesResponseBody() {
     this.article = new ResponseArticle();
   }
+
+  public ResponseArticle getArticle() {
+    return article;
+  }
+
+  public ArticlesResponseBody title(String title) {
+    this.article.title(title);
+    return this;
+  }
+
+  public ArticlesResponseBody description(String description) {
+    this.article.description(description);
+    return this;
+  }
+
+  public ArticlesResponseBody body(String body) {
+    this.article.body(body);
+    return this;
+  }
+
+  public ArticlesResponseBody tagList(ArrayList<String> tagList) {
+    this.article.tagList(tagList);
+    return this;
+  }
+
 }
--- src/test/java/org/peterjaycruz/realworld/api/response/Author.java
+++ src/test/java/org/peterjaycruz/realworld/api/response/Author.java
@@ -2,9 +2,44 @@ package org.peterjaycruz.realworld.api.response;
 
 public class Author {
 
-  public String username;
-  public String bio;
-  public String image;
-  public boolean following;
+  private String username;
+  private String bio;
+  private String image;
+  private boolean following;
 
+  public String getUsername() {
+    return username;
+  }
+
+  public Author username(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public String getBio() {
+    return bio;
+  }
+
+  public Author bio(String bio) {
+    this.bio = bio;
+    return this;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public Author image(String image) {
+    this.image = image;
+    return this;
+  }
+
+  public boolean isFollowing() {
+    return following;
+  }
+
+  public Author following(boolean following) {
+    this.following = following;
+    return this;
+  }
 }
