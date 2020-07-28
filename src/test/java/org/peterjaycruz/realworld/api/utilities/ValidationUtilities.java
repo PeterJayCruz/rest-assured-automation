e3f20847b2bbee4ee46a9cda6508d0af621635b4
PeterJayCruz <pcruz1@mail.sfsu.edu>
2020-07-28 14:35:49 -0700

    validation utilities - added method to validate error response body

--- src/test/java/org/peterjaycruz/realworld/api/utilities/ValidationUtilities.java
+++ src/test/java/org/peterjaycruz/realworld/api/utilities/ValidationUtilities.java
@@ -1,5 +1,13 @@
 package org.peterjaycruz.realworld.api.utilities;
 
+import io.restassured.response.Response;
+import org.peterjaycruz.realworld.api.response.ErrorResponseBody;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValidationUtilities {
 
   public static int validateStatusCode(int actualStatusCode, int expectedStatusCode) {
@@ -11,5 +19,34 @@ public class ValidationUtilities {
     }
 
     return 0;
+  }
+
+  public static int validateErrorResponseBody(Response actualResponse, ErrorResponseBody expectedResponse) {
+    int failCount = 0;
+
+    HashMap<String, ArrayList<String>> actualResponseBodyValues = actualResponse.body().as(ErrorResponseBody.class).getErrors();
+
+    for(Map.Entry<String, ArrayList<String>> entry : expectedResponse.getErrors().entrySet()) {
+      ArrayList<String> actual = actualResponseBodyValues.get(entry.getKey());
+      ArrayList<String> expected = entry.getValue();
+
+      for(int i = 0; i < expected.size(); i++) {
+        if(!actual.get(i).equals(expected.get(i))) {
+          System.out.println("FAILED ERROR MESSAGE VALIDATION: " + entry.getKey());
+          PrintUtilities.printExpectedVsActual(expected.get(i), actual.get(i));
+          failCount++;
+        }
+      }
+    }
+
+    return failCount;
+  }
+
+  public static void analyzeTestCaseResults(int failCount) {
+    Assert.assertEquals(failCount, 0);
+    if(failCount == 0) {
+      System.out.println("Test Result: pass");
+    }
+
   }
 }
